e88a89624a760cbce6dcdc1c861b3472b2b810a3
Hari Sirikonda <harisirikonda22@gmail.com>
2025-01-24 21:07:49 +0530

    Done adding the alerts regarding the inputs and outputs

--- myapp/src/components/Weather.js
+++ myapp/src/components/Weather.js
@@ -10,7 +10,7 @@ import Wind_icon from '../assets/wind.png';
 import './Weather.css';
 
 function Weather() {
-    
+
     const [weatherData, setWeatherData] = useState({
         humidity: null,
         temperature: null,
@@ -21,88 +21,95 @@ function Weather() {
 
     const inputRef = useRef();
     const allIcons = {
-        "01d" : Clear_icon,
-        "01n" : Clear_icon,
-        "02d" : Cloud_icon,
-        "02n" : Cloud_icon,
-        "03d" : Cloud_icon,
-        "03n" : Cloud_icon,
-        "04d" : Drizzle_icon,
-        "04n" : Drizzle_icon,
-        "09d" : Rain_icon,
-        "09n" : Rain_icon,
-        "10d" : Rain_icon,
-        "10n" : Rain_icon,
-        "13d" : Snow_icon,
-        "13n" : Snow_icon
+        "01d": Clear_icon,
+        "01n": Clear_icon,
+        "02d": Cloud_icon,
+        "02n": Cloud_icon,
+        "03d": Cloud_icon,
+        "03n": Cloud_icon,
+        "04d": Drizzle_icon,
+        "04n": Drizzle_icon,
+        "09d": Rain_icon,
+        "09n": Rain_icon,
+        "10d": Rain_icon,
+        "10n": Rain_icon,
+        "13d": Snow_icon,
+        "13n": Snow_icon
     }
 
     const search = async (city) => {
+        if (city === "") {
+            alert("Enter a valid City Name");
+            return;
+        }
         try {
             const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${process.env.REACT_APP_API_KEY}`;
-            const responce = await fetch(url);
-            const data = await responce.json();
-            console.log(data);
-            const icon = allIcons[data.weather[0].icon] || Clear_icon;
+            const response = await fetch(url);
+            const data = await response.json();
+            if (!response.ok) {
+                alert(data.message);
+                return;
+            }
+            const icon = data.weather && data.weather[0] ? allIcons[data.weather[0].icon] : Clear_icon;
             setWeatherData({
-                humidity : data.main.humidity,
-                temperature : Math.floor(data.main.temp),
-                location : data.name,
-                windSpeed : data.wind.speed,
-                icon : icon
+                humidity: data.main.humidity,
+                temperature: Math.floor(data.main.temp),
+                location: data.name,
+                windSpeed: data.wind.speed,
+                icon: icon
             })
         } catch (error) {
+            setWeatherData(false);
             console.error("Error fetching weather data:", error);
         }
     }
 
-    useEffect(()=>{
+    useEffect(() => {
         search("London");
-    },[])
+    }, [])
 
     return (
         <div className='weather-container shadow m-3 p-3 rounded'>
-            {/* Search input area */}
             <div className='d-flex justify-content-center align-items-center'>
                 <input className='search-input form-control m-2 rounded-pill shadow-none' ref={inputRef} placeholder='Enter city name' type='text'></input>
                 <button className='search-button btn btn-light m-2 rounded-pill shadow-none' onClick={() => search(inputRef.current.value)}><b>Search</b></button>
             </div>
-            {/* Temperature area */}
-            <div className='container text-center'>
-                <img src={weatherData.icon} alt='show me'/>
-                <h3>{weatherData.temperature}&deg; Celsius</h3>
-                <h1 className='text-light'>{weatherData.location}</h1>
-            </div>
-            {/* Humidity and Wind speed area */}
-            <div className='container-fluid d-flex align-items-center justify-content-center m-2 p-2'>
-                <div className='container mt-3 text-center'>
-                    <div className='containerfluid d-flex align-items-center justify-content-center'>
-                        <div>
-                            <img src={Humidity_icon} alt='show me'/>
+            {weatherData ? <>
+
+                <div className='container text-center'>
+                    <img src={weatherData.icon} alt='show me' />
+                    <h3>{weatherData.temperature}&deg; Celsius</h3>
+                    <h1 className='text-light'>{weatherData.location}</h1>
+                </div>
+                <div className='container-fluid d-flex align-items-center justify-content-center m-2 p-2'>
+                    <div className='container mt-3 text-center'>
+                        <div className='containerfluid d-flex align-items-center justify-content-center'>
+                            <div>
+                                <img src={Humidity_icon} alt='show me' />
+                            </div>
+                            <div className='mx-2'>
+                                <h1>{weatherData.humidity}%</h1>
+                            </div>
                         </div>
-                        <div className='mx-2'>
-                            <h1>{weatherData.humidity}%</h1>
+                        <div className='container'>
+                            <h4>Humidity</h4>
                         </div>
                     </div>
-                    <div className='container'>
-                        <h4>Humidity</h4>
+                    <div className='container mt-3 text-center'>
+                        <div className='containerfluid d-flex align-items-center justify-content-center'>
+                            <div>
+                                <img src={Wind_icon} alt='show me' />
+                            </div>
+                            <div className='mx-2'>
+                                <h1>{weatherData.windSpeed} km/h</h1>
+                            </div>
+                        </div>
+                        <div className='container'>
+                            <h4>Wind Speed</h4>
+                        </div>
                     </div>
                 </div>
-                {/* Wind speed area */}
-                <div className='container mt-3 text-center'>
-                    <div className='containerfluid d-flex align-items-center justify-content-center'>
-                        <div>
-                            <img src={Wind_icon} alt='show me'/>
-                        </div>
-                        <div className='mx-2'>
-                            <h1>{weatherData.windSpeed} km/h</h1>
-                        </div>
-                    </div>
-                    <div className='container'>
-                        <h4>Wind Speed</h4>
-                    </div>
-                </div>
-            </div>
+            </> : <></>}
         </div>
     )
 }
